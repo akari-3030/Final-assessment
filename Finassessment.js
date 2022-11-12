6051309622735ef86dee46adf448cb6440367420
akari-3030 <113843205+akari-3030@users.noreply.github.com>
2022-11-12 21:57:25 +0900

    My気分YouTubeの追加

---
+++ Finassessment.js
@@ -0,0 +1,48 @@
+function OnButtonClick1(){
+    rand = Math.floor(Math.random()*5);
+	if (rand == 0) msg = location.href="https://www.youtube.com/watch?v=lh-zXM2udb0"
+	if (rand == 1) msg = location.href="https://www.youtube.com/watch?v=04G61OP0l9Q"
+	if (rand == 2) msg = location.href="https://www.youtube.com/watch?v=fbE1AxwbaJs"
+	if (rand == 3) msg = location.href="https://www.youtube.com/watch?v=97ipmpinpbU"
+	if (rand == 4) msg = location.href="https://www.youtube.com/watch?v=__uOlOneKiI"
+}
+function OnButtonClick2(){
+    rand = Math.floor(Math.random()*5);
+	if (rand == 0) msg = location.href= "https://www.youtube.com/watch?v=_hMcSbl9nFI"
+	if (rand == 1) msg = location.href= "https://www.youtube.com/watch?v=dAKQ5p-b72I"
+	if (rand == 2) msg = location.href= "https://www.youtube.com/watch?v=QXnbf9lIUGg"
+	if (rand == 3) msg = location.href= "https://www.youtube.com/watch?v=HFFWE_8xZAg"
+	if (rand == 4) msg = location.href= "https://www.youtube.com/watch?v=EyMT7R8WhiU"
+}
+function OnButtonClick3(){
+    rand = Math.floor(Math.random()*5);
+	if (rand == 0) msg = location.href="https://www.youtube.com/watch?v=VGAyK4nZEkY"
+	if (rand == 1) msg = location.href="https://www.youtube.com/watch?v=mw6H6M7e_eY"
+	if (rand == 2) msg = location.href="https://www.youtube.com/watch?v=WPVRRO3cY2s"
+	if (rand == 3) msg = location.href="https://www.youtube.com/watch?v=vxT7H9wVSHQ"
+	if (rand == 4) msg = location.href="https://www.youtube.com/watch?v=kg1kM8SCp5w"
+}
+function OnButtonClick4(){
+    rand = Math.floor(Math.random()*5);
+	if (rand == 0) msg = location.href="https://www.youtube.com/watch?v=-S4O0DkBEWQ"
+	if (rand == 1) msg = location.href="https://www.youtube.com/watch?v=ziLOhc0TqB0"
+	if (rand == 2) msg = location.href="https://www.youtube.com/watch?v=akRqB_3MHDc"
+	if (rand == 3) msg = location.href="https://www.youtube.com/watch?v=tlavMss7C1E"
+	if (rand == 4) msg = location.href="https://www.youtube.com/watch?v=Mndl0_yB9Go"
+}
+function OnButtonClick5(){
+    rand = Math.floor(Math.random()*5);
+	if (rand == 0) msg = location.href="https://www.youtube.com/watch?v=xdJ0LgydMdU"
+	if (rand == 1) msg = location.href="https://www.youtube.com/watch?v=YuSRsWLnhFA"
+	if (rand == 2) msg = location.href="https://www.youtube.com/watch?v=USx3nxa2ktk"
+	if (rand == 3) msg = location.href="https://www.youtube.com/watch?v=oJ5IWzZEZ_M"
+	if (rand == 4) msg = location.href="https://www.youtube.com/watch?v=X27uaBNz2Ns"
+}
+function OnButtonClick6(){
+    rand = Math.floor(Math.random()*5);
+	if (rand == 0) msg = location.href="https://www.youtube.com/watch?v=aR9zYyDuadk"
+	if (rand == 1) msg = location.href="https://www.youtube.com/watch?v=S9VbU0lPEyE"
+	if (rand == 2) msg = location.href="https://www.youtube.com/watch?v=xhqsgyC9Hwk"
+	if (rand == 3) msg = location.href="https://www.youtube.com/watch?v=a5IZFkbMIac"
+	if (rand == 4) msg = location.href="https://www.youtube.com/watch?v=SbEoiPwADM4"
+}
